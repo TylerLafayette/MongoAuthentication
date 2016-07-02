d0856292c2ee5ce10011076d7ecc8ee184c25aa9
Tyler Lafayette <Tyler@Tylers-MacBook-Pro.local>
2016-07-01 19:50:16 -0700

    Added starting test items

--- module.js
+++ module.js
@@ -1,3 +1,20 @@
+var authenticationDb;
+var authMongoClient = require('mongodb').MongoClient;
+
+exports.connect = function(connString, callback){
+	authMongoClient.connect(connString, function(err, db) {
+		if(err) {
+			console.log("<MongoAuthentication> MongoDB connection error: " + err.msg);
+		}else {
+			console.log("<MongoAuthentication> Successfully connected to your MongoDB database!");
+			authenticationDb = db;
+			callback();
+		}
+	});
+}
+
 exports.test = function(){
-	console.log("MongoAuth is running!!! :)");
+	if(authenticationDb) {
+		console.log("MongoAuth is running!!! :)");
+	}
 }
